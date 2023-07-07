8f47debe517d7a49f585ee6134c29f001ceb6b2b
JohnOffice <montemarjohn66@gmail.com>
2023-07-07 15:43:02 +0800

    Add db

--- db/info_cast.sql
+++ db/info_cast.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2023 at 09:59 AM
+-- Generation Time: Jul 07, 2023 at 09:41 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `course`
 --
 DROP TABLE IF EXISTS `course`;
+
 CREATE TABLE `course` (
   `course_id` int(11) NOT NULL,
   `name` varchar(250) NOT NULL,
@@ -34,12 +35,23 @@ CREATE TABLE `course` (
   `status` enum('active','inactive') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `name`, `acronym`, `status`) VALUES
+(1, 'Bachelor Of Science In Information Technology', 'BSIT', 'active'),
+(2, 'Bachelor Of Science In Hospitality Management', 'BSHM', 'active'),
+(3, 'Bachelor In Elementary Education', 'BEED', 'active'),
+(4, 'Bachelor Of Secondary Education', 'BSEd', 'active');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 DROP TABLE IF EXISTS `users`;
+
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fname` varchar(250) NOT NULL,
@@ -52,19 +64,21 @@ CREATE TABLE `users` (
   `email` varchar(250) NOT NULL,
   `contact` varchar(32) DEFAULT NULL,
   `role` enum('student','teacher','admin','alumni') NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `avatar` varchar(250) DEFAULT NULL,
   `password` varchar(500) DEFAULT NULL,
-  `isNew` tinyint(1) NOT NULL DEFAULT 0
+  `isNew` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `set_graduate_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fname`, `mname`, `lname`, `course_id`, `year`, `section`, `sy`, `email`, `contact`, `role`, `created_at`, `avatar`, `password`, `isNew`) VALUES
-(1, 'Admin', NULL, 'Admin', NULL, NULL, NULL, NULL, 'admin@admin.com', NULL, 'admin', '2023-07-04 23:10:02', NULL, '$argon2i$v=19$m=65536,t=4,p=1$RUZKYi8vdEZPZTIzRGJlSQ$nu99vTpWpkgmgcqbXBHo9g07+JvQLrCuupNq0t3Kkw4', 0),
-(4, 'Test', 'Test', 'Montemar', NULL, NULL, NULL, NULL, 'montemar@gmail.com', '098653', 'admin', '2023-07-06 00:19:41', NULL, NULL, 1);
+INSERT INTO `users` (`id`, `fname`, `mname`, `lname`, `course_id`, `year`, `section`, `sy`, `email`, `contact`, `role`, `avatar`, `password`, `isNew`, `created_at`, `set_graduate_at`) VALUES
+(1, 'Admin', NULL, 'Admin', NULL, NULL, NULL, NULL, 'admin@admin.com', NULL, 'admin', NULL, '$argon2i$v=19$m=65536,t=4,p=1$RUZKYi8vdEZPZTIzRGJlSQ$nu99vTpWpkgmgcqbXBHo9g07+JvQLrCuupNq0t3Kkw4', 0, '2023-07-04 23:10:02', NULL),
+(6, 'Awd', 'Awd', 'Awd', 1, 4, 'A', '2022-23', 'test@email.com', '0987654321', 'alumni', NULL, '$argon2i$v=19$m=65536,t=4,p=1$a0NtSkRjYzJkRHVOY3I4Yw$+a7zA6nspFN8UYUhL8IyYN254aqwbjpLBF4AiY6/9wY', 0, '2023-07-07 04:34:13', '2023-07-07'),
+(8, 'Test', 'Test', 'Test', NULL, NULL, NULL, NULL, 'montemar@gmail.com', '098765432', 'admin', '07072023-011404_5996c14479318.jpg', '$argon2i$v=19$m=65536,t=4,p=1$RWhEQkxmTDhZaDFvWXJ3cg$MJT7M5t9H4zntYiiUXP1Aqw3UkFQd/+qnyzrzw6U91Q', 0, '2023-07-07 05:43:39', NULL);
 
 --
 -- Indexes for dumped tables
@@ -80,7 +94,8 @@ ALTER TABLE `course`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `course_id` (`course_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -90,13 +105,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE SET NULL ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
